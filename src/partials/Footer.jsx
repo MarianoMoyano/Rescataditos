d7a5a160505d5c1e44bac7c534c77ca92e124949
Cristian Moyano <cristianmoyano87@hotmail.com>
2023-02-26 19:45:55 -0300

    cambio footer

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -163,6 +163,8 @@ function Footer() {
           </ul>
 
           {/* Copyrights note */}
+          <h6 className="text-gray-800 font-medium mb-2">Rescataditos en adopción</h6>
+
           <div className="text-sm text-gray-600 mr-4">Made by <a className="text-blue-600 hover:underline" href="https://cruip.com/">Cruip</a>. All rights reserved.</div>
           <div className="text-sm text-gray-600 mr-4">
             <a target="_blank" href="https://icons8.com/icon/48841/instagram">Instagram</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
