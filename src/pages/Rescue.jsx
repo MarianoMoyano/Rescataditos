18e98e236e450666155af314c96c8ee0abf4d85b
Cristian Moyano <cristianmoyano87@hotmail.com>
2023-02-26 19:54:43 -0300

    cambio footer y video

--- src/pages/Rescue.jsx
+++ src/pages/Rescue.jsx
@@ -2,8 +2,7 @@ export function Rescue (){
     return(<>
         <div>Rescue</div>
         <video controls>
-            <source src="/src/videos/catchcatpart1.mp4" type="video/mp4" />
-            <source src="/src/videos/catchcatpart2.mp4" type="video/mp4" />
+            <source src="/src/videos/catchcat.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
     </>
